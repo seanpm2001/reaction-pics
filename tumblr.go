fa4eeb13421e34f89c175b9b171514819627b1c4
Albert Wang <albertyw@mit.edu>
2016-03-13 14:49:01 -0700

    Refactor

--- tumblr.go
+++ tumblr.go
@@ -4,12 +4,21 @@ import _ "github.com/joho/godotenv/autoload"
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 	"github.com/MariaTerzieva/gotumblr"
+	"os"
 )
 
 func main() {
+	postsResponse := getPosts()
+	fmt.Println(postsResponse.Posts[0])
+	var post gotumblr.BasePost
+	err := json.Unmarshal(postsResponse.Posts[0], &post)
+	fmt.Println(err)
+	fmt.Println(post)
+	//fmt.Println(postsResponse.Total_posts)
+}
 
+func getPosts() gotumblr.PostsResponse {
 	client := gotumblr.NewTumblrRestClient(
 		os.Getenv("CONSUMER_KEY"),
 		os.Getenv("CONSUMER_SECRET"),
@@ -18,16 +27,9 @@ func main() {
 		"https://www.albertyw.com/",
 		"http://api.tumblr.com",
 	)
-	info := client.Info()
-	fmt.Println(info.User.Name)
 	blogname := "devopsreactions.tumblr.com"
 	blogtypes := "text"
 	options := map[string]string{}
 	postsResponse := client.Posts(blogname, blogtypes, options)
-	fmt.Println(postsResponse.Posts[0])
-	var post gotumblr.BasePost
-	err := json.Unmarshal(postsResponse.Posts[0], &post)
-	fmt.Println(err)
-	fmt.Println(post)
-	//fmt.Println(postsResponse.Total_posts)
+	return postsResponse
 }
