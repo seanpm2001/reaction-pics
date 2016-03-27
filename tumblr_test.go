53ea1c08ce32c32807c8c117eea67c52e525834e
Albert Wang <albertyw@mit.edu>
2016-03-26 22:32:30 -0700

    Add mock postsresponse

--- tumblr_test.go
+++ tumblr_test.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/MariaTerzieva/gotumblr"
 	"testing"
 )
 
-var mockBlogInfo = gotumblr.BlogInfo{
-	Title:       "title",
-	Posts:       10,
-	Name:        "name",
-	Url:         "url",
-	Updated:     5,
-	Description: "description",
-	Ask:         true,
-	Ask_anon:    false,
-	Likes:       50,
+func makeMockPost() gotumblr.PostsResponse {
+	mockBlogInfo := gotumblr.BlogInfo{
+		Title: "title",
+		Posts: 10,
+		Name:  "name",
+		Url:   "url",
+	}
+	mockTextPost := gotumblr.TextPost{
+		Title: "title",
+		Body:  "Body",
+	}
+	mockTextPostJson, _ := json.Marshal(&mockTextPost)
+	mockTextPostJsonRawMessage := *(*json.RawMessage)(&mockTextPostJson)
+	mockPostsResponse := gotumblr.PostsResponse{
+		Blog:        mockBlogInfo,
+		Posts:       []json.RawMessage{mockTextPostJsonRawMessage},
+		Total_posts: 5,
+	}
+	return mockPostsResponse
 }
 
 func TestParsePosts(t *testing.T) {
