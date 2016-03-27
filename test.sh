7e51aab9b0de23c57347b16f7345d208d208fe1f
Albert Wang <albertyw@mit.edu>
2016-03-27 14:28:25 -0700

    Use path glide

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd -P `pwd`
-go test -v $(./glide novendor)
+go test -v $(glide novendor)
 
 rm -rf vendor
 
@@ -11,25 +11,25 @@ VET_ERRORS=$(go vet .)
 if [ -n "$VET_ERRORS" ]; then
     echo "Lint failures on:"
     echo "$VET_ERRORS"
-    ./glide install
+    glide install
     exit 1
 fi
 
-FMT_ERRORS=$(gofmt -e -l -d $(./glide novendor))
+FMT_ERRORS=$(gofmt -e -l -d $(glide novendor))
 if [ -n "$FMT_ERRORS" ]; then
     echo "Fmt failures on:"
     echo "$FMT_ERRORS"
-    ./glide install
+    glide install
     exit 1
 fi
 
 go get -u github.com/golang/lint/golint
-LINT_ERRORS=$(golint $(./glide novendor))
+LINT_ERRORS=$(golint $(glide novendor))
 if [ -n "$LINT_ERRORS" ]; then
     echo "Lint failures on:"
     echo "$LINT_ERRORS"
-    ./glide install
+    glide install
     exit 1
 fi
 
-./glide install
+glide install
