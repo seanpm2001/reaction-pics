d6364fa3ae012f37576ad64ee0fa132ad11ad3dc
Albert Wang <albertyw@mit.edu>
2016-10-16 00:12:11 -0700

    Add test for getRow

--- tumblr/csv_test.go
+++ tumblr/csv_test.go
@@ -1,1 +1,23 @@
 package tumblr
+
+import (
+	"testing"
+)
+
+func TestGetRow(t *testing.T) {
+	post := Post{
+		1234,
+		"title",
+		"url",
+	}
+	row := getRow(post)
+	if row[0] != "1234" {
+		t.Fail()
+	}
+	if row[1] != "title" {
+		t.Fail()
+	}
+	if row[2] != "url" {
+		t.Fail()
+	}
+}
