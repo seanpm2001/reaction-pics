a2cddfcce039158992e6adc591a41e30af2ea18c
Albert Wang <albertyw@mit.edu>
2017-01-05 03:09:44 +0900

    Fix go http being too lenient with url pattern matching

--- server/main.go
+++ server/main.go
@@ -15,6 +15,20 @@ var indexPath = fmt.Sprintf("%s/index.htm", templateDir)
 var uRLFilePaths = map[string]func() (string, error){}
 var posts []tumblr.Post
 
+func exactURL(
+	targetFunc func(http.ResponseWriter, *http.Request),
+	requestedPath string,
+) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != requestedPath {
+			http.NotFound(w, r)
+			return
+		}
+		targetFunc(w, r)
+		return
+	}
+}
+
 func readFile(p string) func(http.ResponseWriter, *http.Request) {
 	path := p
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -37,7 +51,7 @@ func Run(p []tumblr.Post) {
 	posts = p
 	address := ":" + os.Getenv("PORT")
 	fmt.Println("server listening on", address)
-	http.HandleFunc("/", readFile(indexPath))
+	http.HandleFunc("/", exactURL(readFile(indexPath), "/"))
 	http.HandleFunc(dataURLPath, dataURLHandler)
 	http.ListenAndServe(address, nil)
 }
