4c6569f51dbd5a89114aee94078d127544eeb712
Albert Wang <albertyw@mit.edu>
2017-01-05 02:10:54 +0900

    Add tests

--- server/main_test.go
+++ server/main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestGetURLHandler(t *testing.T) {
+func TestGetRootURLHandler(t *testing.T) {
 	handler, err := getURLHandler("/")
 	if err != nil {
 		t.Fail()
@@ -16,15 +16,38 @@ func TestGetURLHandler(t *testing.T) {
 	if len(data) == 0 {
 		t.Fail()
 	}
-	handler, err = getURLHandler("/asdf")
+}
+
+func TestGetDataURLHandler(t *testing.T) {
+	handler, err := getURLHandler("/data.json")
+	if err != nil {
+		t.Fail()
+	}
+	if handler == nil {
+		t.Fail()
+	}
+	data, err := handler()
+	if err != nil {
+		t.Fail()
+	}
+	if data != "null" {
+		t.Fail()
+	}
+}
+
+func TestGetUnknownURLHandler(t *testing.T) {
+	handler, err := getURLHandler("/asdf")
 	if err == nil {
 		t.Fail()
 	}
 	if handler == nil {
 		t.Fail()
 	}
-	data, err = handler()
+	data, err := handler()
 	if err == nil {
+		t.Fail()
+	}
+	if data != "" {
 		t.Fail()
 	}
 }
