7959e43d8a1dd0e4b53bde56377edd3d33cbcaeb
Albert Wang <albertyw@mit.edu>
2017-01-23 00:45:25 -0800

    Add tests for server package

--- server/main_test.go
+++ server/main_test.go
@@ -1,3 +1,64 @@
 package server
 
-import ()
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestReadFile(t *testing.T) {
+	handler := readFile(indexPath)
+	request, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fail()
+	}
+	response := httptest.NewRecorder()
+	handler(response, request)
+	if response.Code != 200 {
+		t.Fail()
+	}
+	if len(response.Body.String()) < 100 {
+		t.Fail()
+	}
+}
+
+func TestExactURL(t *testing.T) {
+	handler := exactURL(readFile(indexPath), "/")
+	request, err := http.NewRequest("GET", "/asdf", nil)
+	if err != nil {
+		t.Fail()
+	}
+	response := httptest.NewRecorder()
+	handler(response, request)
+	if response.Code != 404 {
+		t.Fail()
+	}
+}
+
+func TestNoExactURL(t *testing.T) {
+	handler := readFile(indexPath)
+	request, err := http.NewRequest("GET", "/asdf", nil)
+	if err != nil {
+		t.Fail()
+	}
+	response := httptest.NewRecorder()
+	handler(response, request)
+	if response.Code != 200 {
+		t.Fail()
+	}
+}
+
+func TestDataURLHandler(t *testing.T) {
+	request, err := http.NewRequest("GET", "/data.json", nil)
+	if err != nil {
+		t.Fail()
+	}
+	response := httptest.NewRecorder()
+	dataURLHandler(response, request)
+	if response.Code != 200 {
+		t.Fail()
+	}
+	if response.Body.String() != "null" {
+		t.Fail()
+	}
+}
