c00ca9a431ac0402628b6e87cb9e00e3f48856bb
Albert Wang <albertyw@mit.edu>
2017-04-03 01:16:20 -0700

    Add some css

--- server/templates/app.js
+++ server/templates/app.js
@@ -11,8 +11,8 @@ function updateResults() {
       $("#results").html("");
       for (var x=0; x<data.length; x++) {
         var postId = 'post' + x;
-        var post = '<div id="' + postId + '">';
-        post += '<a href="' + data[x].url + '">' + data[x].title + '</a><br />';
+        var post = '<div id="' + postId + '" class="result">';
+        post += '<h2><a href="' + data[x].url + '">' + data[x].title + '</a></h2>';
         post += '</div>';
         var timeout = showImage(x, postId, data[x].image);
         timeouts.push(timeout);
@@ -24,7 +24,7 @@ function updateResults() {
 
 function showImage(x, postId, image) {
   var timeout = setTimeout(function() {
-    var img = '<img src="' + image + '" />';
+    var img = '<img src="' + image + '" class="result-img"/>';
     $("#" + postId).append(img);
   }, 500 * x);
   return timeout;
