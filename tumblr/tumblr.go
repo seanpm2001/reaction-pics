9fb96012600d681689a7c52e7f7c93198a8013ff
Albert Wang <albertyw@mit.edu>
2017-05-01 01:07:12 +0800

    Fix #10 - Add life of a software developer

--- tumblr/tumblr.go
+++ tumblr/tumblr.go
@@ -20,6 +20,7 @@ const (
 // Blogs is a list of all blogs to read from
 var Blogs = []string{
 	"devopsreactions.tumblr.com",
+	"lifeofasoftwareengineer.tumblr.com",
 }
 
 func duplicateChan(in <-chan Post, out1, out2 chan<- Post, wg *sync.WaitGroup) {
