e83edc6650f7a1b8e60e399b551cc1c2cb3e1748
Albert Wang <git@albertyw.com>
2018-09-20 07:32:07 +0000

    Bind static files into docker container

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,7 +10,9 @@ git pull
 docker build -t reaction-pics:production .
 docker stop reaction-pics || echo
 docker container prune -f
-docker run --detach --restart always -p 127.0.0.1:5003:5003 --name reaction-pics reaction-pics:production
+docker run --detach --restart always -p 127.0.0.1:5003:5003 \
+    --mount type=bind,source="$(pwd)"/tumblr/data,target=/root/gocode/src/github.com/albertyw/reaction-pics/tumblr/data \
+    --name reaction-pics reaction-pics:production
 
 # Cleanup docker
 docker image prune -f
