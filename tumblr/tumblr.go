74e1aa96074294af4a840bd14ebcd88958fae70a
Albert Wang <git@albertyw.com>
2018-09-22 19:31:18 -0700

    Remove call to WritePosts

--- tumblr/tumblr.go
+++ tumblr/tumblr.go
@@ -26,26 +26,14 @@ func duplicateChan(in <-chan Post, out1, out2 chan<- Post, wg *sync.WaitGroup) {
 }
 
 // GetPosts returns a list of all Posts
-func GetPosts(out1 chan<- Post) {
-	var wg sync.WaitGroup
-	channels := [](chan<- Post){out1}
+func GetPosts(posts chan<- Post) {
+	defer func() { close(posts) }()
 	for _, blogName := range Blogs {
-		posts := make(chan Post)
-		out2 := make(chan Post)
-		channels = append(channels, out2)
-		wg.Add(1)
-		go duplicateChan(posts, out1, out2, &wg)
-		go getBlogPosts(blogName, posts)
-		go WritePostsToCSV(blogName, out2)
-	}
-	wg.Wait()
-	for _, c := range channels {
-		close(c)
+		getBlogPosts(blogName, posts)
 	}
 }
 
 func getBlogPosts(blogName string, posts chan<- Post) {
-	defer func() { close(posts) }()
 	existingPosts := ReadPostsFromCSV(blogName)
 	maxPostID := int64(0)
 	for _, p := range existingPosts {
