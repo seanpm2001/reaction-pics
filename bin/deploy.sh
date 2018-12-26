74d92eac057055ef3dfd9e29f7e576dcfdbc0cef
Albert Wang <git@albertyw.com>
2018-12-26 20:44:13 +0800

    Do not prune containers less than 2 weeks old

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,13 +11,13 @@ git pull
 # Build and start container
 docker build -t reaction-pics:production .
 docker stop reaction-pics || echo
-docker container prune -f
+docker container prune --force --filter "until=336h"
 docker run --detach --restart always -p 127.0.0.1:5003:5003 \
     --mount type=bind,source="$(pwd)"/tumblr/data,target=/root/gocode/src/github.com/albertyw/reaction-pics/tumblr/data \
     --name reaction-pics reaction-pics:production
 
 # Cleanup docker
-docker image prune -f
+docker image prune --force --filter "until=336h"
 
 # Update nginx
 sudo service nginx reload
