0522355dd8df6ea056f9e9d458ecd8d5c75eabb3
Albert Wang <git@albertyw.com>
2019-02-18 00:51:20 -0800

    Add test for setupEnv

--- main_test.go
+++ main_test.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestSetupEnv(t *testing.T) {
+	setupEnv()
+	port := os.Getenv("PORT")
+	assert.NotEqual(t, port, "")
+}
 
 func TestGetNewRelicApp(t *testing.T) {
 	setupEnv()
