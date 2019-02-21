42b6ebefb942f5978ac4d2d0638b1a2e1c664cd8
Albert Wang <git@albertyw.com>
2019-02-21 01:06:41 -0800

    Remove unneeded maxPostID logic

--- tumblr/tumblr.go
+++ tumblr/tumblr.go
@@ -4,11 +4,7 @@ package tumblr
 func GetPosts(posts chan<- Post) {
 	defer func() { close(posts) }()
 	existingPosts := ReadPostsFromCSV()
-	maxPostID := int64(0)
 	for _, p := range existingPosts {
-		if p.ID > maxPostID {
-			maxPostID = p.ID
-		}
 		posts <- p
 	}
 }
