f49585cc29f5d2059b30ff921cf08b6dfb18c8c4
Albert Wang <git@albertyw.com>
2019-02-21 01:43:06 -0800

    Pass in test bool to select csv to read

--- tumblr/csv.go
+++ tumblr/csv.go
@@ -7,11 +7,12 @@ import (
 
 const (
 	prodCSVPath = "tumblr/data/posts.csv"
+	testCSVPath = "tumblr/data/posts_test.csv"
 )
 
 // ReadPostsFromCSV reads a CSV file into a list of posts
-func ReadPostsFromCSV() (posts []Post) {
-	file, err := os.Open(getCSVPath())
+func ReadPostsFromCSV(csvPath string) (posts []Post) {
+	file, err := os.Open(csvPath)
 	defer file.Close()
 	if err != nil {
 		return
@@ -28,8 +29,11 @@ func ReadPostsFromCSV() (posts []Post) {
 	return posts
 }
 
-func getCSVPath() string {
+func getCSVPath(test bool) string {
 	path := prodCSVPath
+	if test {
+		path = testCSVPath
+	}
 	path = os.Getenv("ROOT_DIR") + "/" + path
 	return path
 }
--- tumblr/csv_test.go
+++ tumblr/csv_test.go
@@ -1,26 +1,13 @@
 package tumblr
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func cleanup() {
-	os.Rename(getCSVPath()+".bak", getCSVPath())
-}
-
 func TestReadPostsFromCSV(t *testing.T) {
-	os.Rename(getCSVPath(), getCSVPath()+".bak")
-	defer cleanup()
-
-	data := []byte("1234,title,url,abcd.gif,123")
-	err := ioutil.WriteFile(getCSVPath(), data, 0644)
-	assert.NoError(t, err)
-
-	posts := ReadPostsFromCSV()
+	posts := ReadPostsFromCSV(getCSVPath(true))
 	assert.Equal(t, len(posts), 1)
 	assert.Equal(t, posts[0].ID, int64(1234))
 	assert.Equal(t, posts[0].Title, "title")
