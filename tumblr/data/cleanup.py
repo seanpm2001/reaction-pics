fd84ff57aeb72d2a61c62f71682a7c36b70e287f
Albert Wang <git@albertyw.com>
2019-02-26 21:21:06 -0800

    Deduplicate images

--- tumblr/data/cleanup.py
+++ tumblr/data/cleanup.py
@@ -5,18 +5,13 @@ with open('duplicates.json', 'r') as handle:
     duplicates = json.loads(handle.read())
 
 with open('posts.csv', 'r') as handle:
-    posts = handle.readlines()
+    posts = handle.read()
 
 for duplicate in duplicates:
-    if duplicate[0] != '03a0e98e-0f1a-46d8-99e9-67d01094bafa.gif':
-        continue
-    for d in duplicate:
-        print(d)
-        posts = [l for l in posts if d not in l]
-        try:
-            os.remove('static/' + d)
-        except FileNotFoundError:
-            pass
+    target = duplicate[0]
+    for d in duplicate[1:]:
+        posts = posts.replace(d, target)
+        os.remove('static/' + d)
 
 with open('posts.csv', 'w') as handle:
-    handle.write(''.join(posts))
+    handle.write(posts)
