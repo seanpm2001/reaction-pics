d2a829522be146e7cdf8b9a61e12c5a84de8def7
Albert Wang <git@albertyw.com>
2019-04-28 21:05:59 -0700

    Add docker networking

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,11 +10,17 @@ git pull
 
 # Build and start container
 docker build -t reaction-pics:production .
+docker network inspect "reaction-pics" &>/dev/null ||
+    docker network create --driver bridge "reaction-pics"
 docker stop reaction-pics || echo
 docker container prune --force --filter "until=336h"
 docker container rm reaction-pics
-docker run --detach --restart always -p 127.0.0.1:5003:5003 \
-    --name reaction-pics reaction-pics:production
+docker run \
+    --detach \
+    --restart=always \
+    --publish="127.0.0.1:5003:5003" \
+    --network="reaction-pics" \
+    --name=reaction-pics reaction-pics:production
 
 # Cleanup docker
 docker image prune --force --filter "until=336h"
