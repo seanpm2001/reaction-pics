53a45af0b17c7def51f7f9332f1f411a7783e6d9
Albert Wang <git@albertyw.com>
2019-05-14 23:09:02 -0700

    Add test

--- tumblr/csv_test.go
+++ tumblr/csv_test.go
@@ -1,6 +1,7 @@
 package tumblr
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,4 +15,10 @@ func TestReadPostsFromCSV(t *testing.T) {
 	assert.Equal(t, posts[0].URL, "url")
 	assert.Equal(t, posts[0].Image, "http://static.reaction.pics/img/abcd.gif")
 	assert.Equal(t, posts[0].Likes, int64(123))
+}
+
+func TestReadPercentFromCSV(t *testing.T) {
+	data := `1234,a% b,url,image,123`
+	posts := readCSV(strings.NewReader(data))
+	assert.Equal(t, posts[0].Title, "a% b")
 }
