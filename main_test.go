d2473a5585ce19339eebf634ac346de962b4d7a3
Albert Wang <git@albertyw.com>
2019-12-26 22:13:45 +0800

    Add tests for getLogger

--- main_test.go
+++ main_test.go
@@ -26,3 +26,16 @@ func TestSetupRollbar(t *testing.T) {
 	assert.Equal(t, rollbar.Token(), os.Getenv("ROLLBAR_SERVER_TOKEN"))
 	assert.Equal(t, rollbar.Environment(), os.Getenv("ENVIRONMENT"))
 }
+
+func TestGetLogger(t *testing.T) {
+	origEnv := os.Getenv("ENVIRONMENT")
+	defer func() { os.Setenv("ENVIRONMENt", origEnv) }()
+
+	os.Setenv("ENVIRONMENT", "development")
+	logger := getLogger()
+	assert.NotNil(t, logger)
+
+	os.Setenv("ENVIRONMENT", "production")
+	logger = getLogger()
+	assert.NotNil(t, logger)
+}
