5973ec0800191f93f9dd3a1d31bc258909745041
Albert Wang <git@albertyw.com>
2020-12-28 22:00:13 -0800

    Tree-shake built JS blob

--- bin/browserify.js
+++ bin/browserify.js
@@ -9,6 +9,7 @@ const outputFile = path.join(__dirname, '..', 'server', 'static', 'app.js');
 
 browserify(inputFile, {debug: true})
   .transform('envify')
+  .plugin('common-shakeify')
   .transform('babelify',  {presets: ['@babel/preset-env']})
   .transform('uglifyify', {
     mangle: false,
