48988ac0976571a44feaef4762f703814cab5d74
Albert Wang <git@albertyw.com>
2020-12-28 21:57:45 -0800

    Keep JS function argument names the same after minification

--- bin/browserify.js
+++ bin/browserify.js
@@ -10,6 +10,11 @@ const outputFile = path.join(__dirname, '..', 'server', 'static', 'app.js');
 browserify(inputFile, {debug: true})
   .transform('envify')
   .transform('babelify',  {presets: ['@babel/preset-env']})
-  .transform('uglifyify', {compress: true, 'keep_fnames': true, global: true})
+  .transform('uglifyify', {
+    mangle: false,
+    toplevel: true,
+    keep_fnames: true,
+    keep_classnames: true,
+  })
   .bundle()
   .pipe(fs.createWriteStream(outputFile));
