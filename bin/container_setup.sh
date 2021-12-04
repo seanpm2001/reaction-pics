1f15a984a66e0360e0d67537d5b434d223fe2866
Albert Wang <git@albertyw.com>
2021-12-03 22:23:33 -0500

    Remove unused dep installation

--- bin/container_setup.sh
+++ bin/container_setup.sh
@@ -7,9 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR/.." || exit 1
 
 # Compile go and node
-mkdir /root/gocode/bin
-mkdir /root/gocode/pkg
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-export PATH="$PATH:$GOPATH/bin"
 make bins
 npm prune --production
