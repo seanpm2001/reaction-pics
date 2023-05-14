78f115ce012a39e5efceafcbfa99268ea0a48841
Albert Wang <git@albertyw.com>
2023-05-14 01:36:02 -0700

    Add code to update posts.csv

--- sigsegmeme/download_files.py
+++ sigsegmeme/download_files.py
@@ -19,12 +19,19 @@ class CSVData:
     def url(self) -> str:
         return 'https://twitter.com/sigsegmeme/status/%s' % (self.tweet_id)
 
-    def internal_image_location(self) -> str:
+    def image_name(self) -> str:
         if not self.media_urls:
             return ''
         extension = self.media_urls.split('.')[-1].split('?')[0]
-        filename = './media/%s.%s' % (self.tweet_id, extension)
+        filename = '%s.%s' % (self.tweet_id, extension)
         return filename
+
+    def internal_image_location(self) -> str:
+        image_name = self.image_name()
+        if not image_name:
+            return ''
+        filepath = './media/%s' % image_name
+        return filepath
 
     def download_tweet(self) -> None:
         tweet_location = self.internal_image_location()
@@ -49,11 +56,26 @@ def parse_csv() -> list[CSVData]:
     return data
 
 
+def write_to_master_csv(csv_data: list[CSVData]) -> None:
+    with open('../model/data/posts.csv', 'a', newline='') as handle:
+        csv_writer = csv.writer(handle, dialect='unix', quoting=csv.QUOTE_MINIMAL)
+        for data in csv_data:
+            row = [
+                data.tweet_id,
+                data.text,
+                data.url(),
+                data.image_name(),
+                data.favorites,
+            ]
+            csv_writer.writerow(row)
+
+
 def main() -> None:
     csv_data = parse_csv()
-    for row in csv_data:
+    # for row in csv_data:
         # row.download_tweet()
-        print(row.internal_image_location())
+        # print(row.internal_image_location())
+    write_to_master_csv(csv_data)
 
 
 if __name__ == '__main__':
